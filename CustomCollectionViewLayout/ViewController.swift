bb23d990dcea12bd07f7c8a849061a1870b50fd3
Simran Preet Singh <simran5590@gmail.com>
2022-06-19 21:02:48 -0700

    Initial Commit

---
+++ CustomCollectionViewLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCollectionViewLayout
+//
+//  Created by Simran Preet Narang on 2022-06-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
