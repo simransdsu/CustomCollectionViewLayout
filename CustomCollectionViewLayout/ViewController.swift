dbcd33af7a932408771c87dff8a7adc3f8e7a893
Simran Preet Singh <simran5590@gmail.com>
2022-06-19 21:08:00 -0700

    Assets added and created an array for them

--- CustomCollectionViewLayout/ViewController.swift
+++ CustomCollectionViewLayout/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  var dataSource: [String] = (0...5).map { "\($0)" }
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
   }
 
 
